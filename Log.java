154e045cead7597f5eda9cac079e2208447e2d65
Oscar Pinedo <opinedo19@gmail.com>
2018-11-25 03:14:36 -0800

    Create Log.java

---
+++ Log.java
@@ -0,0 +1,25 @@
+class Log{
+
+    public static void main(String[] args){
+        FileReader file = new FileReader("args[0]");
+        String str_parsed = parse(file);  
+      
+    }
+
+
+    static String parse(file){
+        Scanner in = new Scanner(file);
+        String fname;
+        String lname;
+        boolean sand = false;
+        boolean polish = false;
+        boolean eol = false;
+        boolean jig = false;
+        boolean quality = false;
+        while(in.hasNextLine){
+            line = in.nextLine();
+            fname= in.nextLine();
+        }
+    }
+    
+    }
